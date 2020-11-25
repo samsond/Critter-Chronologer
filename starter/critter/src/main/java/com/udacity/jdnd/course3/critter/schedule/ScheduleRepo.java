98745f940f9f14b2ee15b3ba14785eecbf7647b2
samsond <nosmaspoise@gmail.com>
2020-11-25 16:29:05 +0300

    get schedule for employee

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/schedule/ScheduleRepo.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/schedule/ScheduleRepo.java
@@ -1,11 +1,23 @@
 package com.udacity.jdnd.course3.critter.schedule;
 
+import com.udacity.jdnd.course3.critter.user.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Repository
 @Transactional
 public interface ScheduleRepo extends JpaRepository<Schedule, Long> {
+
+
+//    @Query("select s from Schedule s where s.employees = :employee")
+    List<Schedule> findByEmployees(@Param("employees") Employee employee);
+
+    List<Schedule> findAllByEmployeesId(long employeesId);
+
+    List<Schedule> findByEmployees_Id(long employeesId);
 }
--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/schedule/ScheduleService.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/schedule/ScheduleService.java
@@ -1,7 +1,13 @@
 package com.udacity.jdnd.course3.critter.schedule;
 
+import com.udacity.jdnd.course3.critter.user.Employee;
+import com.udacity.jdnd.course3.critter.user.EmployeeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ScheduleService {
@@ -9,7 +15,25 @@ public class ScheduleService {
     @Autowired
     private ScheduleRepo scheduleRepo;
 
+    @Autowired
+    private EmployeeRepo employeeRepo;
+
     public Schedule createSchedule(Schedule schedule) {
         return scheduleRepo.save(schedule);
+    }
+
+    public List<Schedule> findScheduleForEmployee(long id) {
+        Optional<Employee> optionalEmployee = employeeRepo.findById(id);
+        Employee employee = employeeRepo.getOne(id);
+
+        List<Schedule> schedules = scheduleRepo.findByEmployees(employee);
+//        if (optionalEmployee.isPresent()) {
+//            schedules = scheduleRepo.findAllByEmployees(optionalEmployee.get());
+//        } else {
+//            throw new ScheduleNotFoundException("There is no schedule for employee id " + id);
+//        }
+
+
+        return schedules;
     }
 }
