dbe08b2791b3d86cdaede2be1f59f2306370ef85
samsond <nosmaspoise@gmail.com>
2020-11-26 06:42:23 +0300

    add transactional to service layer

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/pet/PetService.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/pet/PetService.java
@@ -6,11 +6,13 @@ import com.udacity.jdnd.course3.critter.user.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Transactional
 public class PetService {
 
     @Autowired
--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/CustomerService.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/CustomerService.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Transactional
 public class CustomerService {
 
     @Autowired
